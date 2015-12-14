12db85ae1e8b1a259797f547410407408a1f5fd2
Jarready <jarready@mail.ustc.edu.cn>
2015-12-14 16:25:13 +0800

    wolff is done

--- wolff.cpp
+++ wolff.cpp
@@ -15,60 +15,56 @@ void ising::wolff(int steps)
     std::uniform_real_distribution<> dis_flip(0, 1);
 
     double p = 1 - std::exp(-2 * J / T);
-    int *pos;  //pos
-    int *u_nei, *d_nei, *l_nei, *r_nei; //neighbors store
-    std::set<int*> C, F_old, F_new;
-
+    int u_x, u_y, d_x, d_y, l_x, l_y, r_x, r_y; //neighbors store
+    int x, y;
+    std::set<int> C, F_old, F_new;
+    //store pos info use int
+    //x*size + y
     for (int step = 0; step < steps; step++)//evolve steps times
     {
-        pos = new int[2];
-        pos[0] = dis_pos(); pos[1] = dis_pos();
-        C.insert(pos); F_old.insert(pos);
+        x = dis_pos(gen); y = dis_pos(gen);
+        C.insert(x * size + y); F_old.insert(x * size + y);
         while (!F_old.empty())
         {
             F_new.clear();
-            u_nei = new int[2]; d_nei = new int[2]; l_nei = new int[2]; r_nei = new int[2];
-            for (std::set<int*>::iterator it = F_old.begin(); it != F_old.end(); it++)
+            for (std::set<int>::iterator it = F_old.begin(); it != F_old.end(); it++)
             {
-                u_nei[0] = mark[(*it)[0]][(*it)[1]].ux; u_nei[1] = mark[(*it)[0]][(*it)[1]].uy;
-                d_nei[0] = mark[(*it)[0]][(*it)[1]].dx; d_nei[1] = mark[(*it)[0]][(*it)[1]].dy;
-                l_nei[0] = mark[(*it)[0]][(*it)[1]].lx; l_nei[1] = mark[(*it)[0]][(*it)[1]].ly;
-                r_nei[0] = mark[(*it)[0]][(*it)[1]].rx; r_nei[1] = mark[(*it)[0]][(*it)[1]].ry;
+                u_x = mark[(*it) / size][(*it) % size].ux; u_y = mark[(*it) / size][(*it) % size].uy;
+                d_x = mark[(*it) / size][(*it) % size].dx; d_y = mark[(*it) / size][(*it) % size].dy;
+                l_x = mark[(*it) / size][(*it) % size].lx; l_y = mark[(*it) / size][(*it) % size].ly;
+                r_x = mark[(*it) / size][(*it) % size].rx; r_y = mark[(*it) / size][(*it) % size].ry;
 
-                if (mark[u_nei[0]][u_nei[1]].spin == mark[(*it)[0]][(*it)[1]].spin   && C.find(u_nei) != C.end() && dis_flip(gen) < p)
+                if (mark[u_x][u_y].spin == mark[(*it) / size][(*it) % size].spin   && C.find(u_x * size + u_y) == C.end() && dis_flip(gen) < p)
                 {
-                    F_new.insert(u_nei);
-                    C.insert(u_nei);
-                    u_nei = new int[2];
+                    F_new.insert(u_x * size + u_y);
+                    C.insert(u_x * size + u_y);
                     //if this neighbor is inserted,
                     //should give set a new pointer to it
                 }
 
-                if (mark[d_nei[0]][d_nei[1]].spin == mark[(*it)[0]][(*it)[1]].spin   && C.find(d_nei) != C.end() && dis_flip(gen) < p)
+                if (mark[d_x][d_y].spin == mark[(*it) / size][(*it) % size].spin   && C.find(d_x * size + d_y) == C.end() && dis_flip(gen) < p)
                 {
-                    F_new.insert(d_nei);
-                    C.insert(d_nei);
-                    d_nei = new int[2];
+                    F_new.insert(d_x * size + d_y);
+                    C.insert(d_x * size + d_y);
                 }
-                if (mark[l_nei[0]][l_nei[1]].spin == mark[(*it)[0]][(*it)[1]].spin   && C.find(l_nei) != C.end() && dis_flip(gen) < p)
+                if (mark[l_x][l_y].spin == mark[(*it) / size][(*it) % size].spin   && C.find(l_x * size + l_y) == C.end() && dis_flip(gen) < p)
                 {
-                    F_new.insert(l_nei);
-                    C.insert(l_nei);
-                    l_nei = new int[2];
+                    F_new.insert(l_x * size + l_y);
+                    C.insert(l_x * size + l_y);
                 }
-                if (mark[r_nei[0]][r_nei[1]].spin == mark[(*it)[0]][(*it)[1]].spin   && C.find(r_nei) != C.end() && dis_flip(gen) < p)
+                if (mark[r_x][r_y].spin == mark[(*it) / size][(*it) % size].spin   && C.find(r_x * size + r_y) == C.end() && dis_flip(gen) < p)
                 {
-                    F_new.insert(r_nei);
-                    C.insert(r_nei);
-                    r_nei = new int[2];
+                    F_new.insert(r_x * size + r_y);
+                    C.insert(r_x * size + r_y);
                 }
                 //end insert neighbor
             }
             F_old = F_new;
         }
-        for (std::set<int*>::iterator it = C.begin(); it != C.end(); it++)
+        for (std::set<int>::iterator it = C.begin(); it != C.end(); it++)
         {
-            mark[(*it)[0]][(*it)[1]].spin = - mark[(*it)[0]][(*it)[1]].spin;
+            //std::cout << (*it)[0] << " " << (*it)[1] << " " << mark[(*it) / size][(*it) % size].spin << "\n";
+            mark[(*it) / size][(*it) % size].spin = - mark[(*it) / size][(*it) % size].spin;
         }
     }
 }
